9f75f8a68ae307119a755f5509015d294d90b3a0
Jeremy Buentello <jb09056@georgiasouthern.edu>
2021-12-28 15:37:43 -0500

    Fixing up the stepper. Fixing some of the disable styles with buttons. Adding watch command so that css files are watched as well as the project.

---
+++ Blatomic.Server/watch.ps1
@@ -0,0 +1,3 @@
+﻿Start-Process -FilePath "npm" -ArgumentList "run watch" -WorkingDirectory "..\Blatomic.Examples"
+Start-Process -FilePath "npm" -ArgumentList "run watch" -WorkingDirectory "..\Blatomic"
+start dotnet watch
