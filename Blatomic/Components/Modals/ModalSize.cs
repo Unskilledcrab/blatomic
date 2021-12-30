0693c1bd0ffd8d8334616287fc9bebd6ff23e3cc
Jeremy Buentello <jb09056@georgiasouthern.edu>
2021-12-30 13:47:19 -0500

    Adding progressbars and modals

---
+++ Blatomic/Components/Modals/ModalSize.cs
@@ -0,0 +1,31 @@
+﻿namespace Blatomic.Components.Modals
+{
+    public enum ModalSize
+    {
+        Small,
+        Medium,
+        Large,
+        Full
+    }
+
+    public static class ModalSizeExtensions
+    {
+        public static string GetStyle(this ModalSize modalSize)
+        {
+            var defaultStyle = "mx-auto sm:w-11/12 md:w-9/12";
+            switch (modalSize)
+            {
+                case ModalSize.Small:
+                    return $"{defaultStyle} lg:w-4/12";
+                case ModalSize.Medium:
+                    return $"{defaultStyle} lg:w-1/2";
+                case ModalSize.Large:
+                    return $"{defaultStyle}";
+                case ModalSize.Full:
+                    return "w-full";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
