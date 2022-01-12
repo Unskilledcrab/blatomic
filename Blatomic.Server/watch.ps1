50a285ba8bf47f422bf3b0858b43c6d06c786a16
Jeremy Buentello <jb09056@georgiasouthern.edu>
2022-01-12 01:23:53 -0500

    Pushing the beginning of the select. Getting rid of the pesky issue of overwriting styles. Modifying the build process to be easier on the devs.

--- Blatomic.Server/watch.ps1
+++ Blatomic.Server/watch.ps1
@@ -1,4 +1,4 @@
 ﻿Start-Process -FilePath "npm" -ArgumentList "run watch" -WorkingDirectory "..\Blatomic.Examples"
-Start-Process -FilePath "npm" -ArgumentList "run watch" -WorkingDirectory "..\Blatomic"
+Start-Process -FilePath "npm" -ArgumentList "run watchstandardcss" -WorkingDirectory "..\Blatomic"
 Start-Process -FilePath "dotnet" -ArgumentList "watch"
 Exit
