affb0bf112abf944c1b5bdcb4356e4a43a838274
Jeremy Buentello <jb09056@georgiasouthern.edu>
2022-01-16 16:07:37 -0500

    Cleaning up darg and drop and making it fully functional for touch devices. Fixing z indexing for the toast container since it should always be on top

--- Blatomic/Threading/ThreadSafeList.cs
+++ Blatomic/Threading/ThreadSafeList.cs
@@ -10,38 +10,52 @@ namespace Blatomic.Threading
         public void LockedAction(Action<List<T>> action)
         {
             lock (_lock)
+            {
                 action(list);
+            }
         }
 
         public TResult LockedFunction<TResult>(Func<List<T>, TResult> func)
         {
             lock (_lock)
+            {
                 return func(list);
+            }
         }
 
         public void Add(T item)
         {
             lock (_lock)
+            {
                 list.Add(item);
+            }
         }
 
         public void Remove(T item)
         {
             lock (_lock)
+            {
                 list.Remove(item);
+            }
         }
 
         public IEnumerable<T> GetItems()
         {
             lock (_lock)
+            {
                 foreach (var item in list)
+                {
                     yield return item;
+                }
+            }
         }
 
         public void Clear()
         {
             lock (_lock)
+            {
                 list.Clear();
+            }
         }
 
         public int Count
@@ -49,50 +63,66 @@ namespace Blatomic.Threading
             get
             {
                 lock (_lock)
+                {
                     return list.Count;
+                }
             }
         }
 
-        public T Find(Predicate<T> match)
+        public T? Find(Predicate<T> match)
         {
             lock (_lock)
+            {
                 return list.Find(match);
+            }
         }
 
         public List<T> FindAll(Predicate<T> match)
         {
             lock (_lock)
+            {
                 return list.FindAll(match);
+            }
         }
 
         public bool Contains(T item)
         {
             lock (_lock)
+            {
                 return list.Contains(item);
+            }
         }
 
         public int IndexOf(T item)
         {
             lock (_lock)
+            {
                 return list.IndexOf(item);
+            }
         }
 
         public T IndexOf(int index)
         {
             lock (_lock)
+            {
                 return list[index];
+            }
         }
 
         public void ForEach(Action<T> action)
         {
             lock (_lock)
+            {
                 list.ForEach(action);
+            }
         }
 
         public void AddRange(IEnumerable<T> collection)
         {
             lock (_lock)
+            {
                 list.AddRange(collection);
+            }
         }
     }
 }
