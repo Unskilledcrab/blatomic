6c7c9d4815ca4e235d3fecc4d19742c1a2de7687
Unskilledcrab <jb09056@georgiasouthern.edu>
2022-01-16 03:22:48 +0000

    Deploying to gh-pages from  @ a8bdf919402363c7acb20a192e9ab5e40c63a8e4 🚀

--- _content/Blatomic/js/WindowService.js
+++ _content/Blatomic/js/WindowService.js
@@ -1,7 +1,27 @@
-﻿function preventDefault(e) {
-    e.preventDefault();
+﻿// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+
+export function TouchDragScrollPrevention(element) {
+    element.addEventListener('touchstart', DisableTouchScroll, wheelOpt);
+    element.addEventListener('touchend', EnableTouchScroll, wheelOpt);
 }
 
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    if (e.cancelable) {
+        e.preventDefault();
+    }
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
     window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
@@ -10,11 +30,18 @@ try {
 } catch (e) { }
 
 var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 export function DisableTouchScroll() {
-    window.addEventListener('touchmove', preventDefault, wheelOpt);
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 export function EnableTouchScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
     window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
