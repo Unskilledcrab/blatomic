1554045271526661aabb762c198d9790f8392aef
Jeremy Buentello <jb09056@georgiasouthern.edu>
2022-04-08 00:56:55 -0400

    Adding more hierarchy extension methods to help hierarchy traversal

--- Blatomic/Extensions/HierarchyExtensions.cs
+++ Blatomic/Extensions/HierarchyExtensions.cs
@@ -22,6 +22,100 @@
 
     public static class HierarchyExtensions
     {
+        public static IEnumerable<IHierarchyNode<TData>> GetAllSiblingNodes<TData>(this IHierarchyNode<TData> node)
+        {
+            if (node.Parent is null)
+            {
+                yield break;
+            }
+
+            foreach (var child in node.Parent.Children)
+            {
+                if (child != node)
+                {
+                    yield return child;
+                }
+            }
+        }
+        public static IEnumerable<TData> GetAllSiblingData<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var sibling in node.GetAllSiblingNodes())
+            {
+                yield return sibling.Data;
+            }
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> GetAllChildrenNodes<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var child in node.Children)
+            {
+                yield return child;
+                foreach (var grandChild in child.GetAllChildrenNodes())
+                {
+                    yield return grandChild;
+                }
+            }
+        }
+        public static IEnumerable<TData> GetAllChildrenData<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var child in node.GetAllChildrenNodes())
+            {
+                yield return child.Data;
+            }
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> GetAllParentNodes<TData>(this IHierarchyNode<TData> node)
+        {
+            if (node.Parent is null)
+            {
+                yield break;
+            }
+
+            yield return node.Parent;
+            foreach (var grandParent in node.Parent.GetAllParentNodes())
+            {
+                yield return grandParent;
+            }
+        }
+        public static IEnumerable<TData> GetAllParentsData<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var parent in node.GetAllParentNodes())
+            {
+                yield return parent.Data;
+            }
+        }
+
+        public static IHierarchyNode<TData> GetRootNode<TData>(this IHierarchyNode<TData> node)
+        {
+            var parentNode = node.Parent;
+            while (parentNode is not null)
+            {
+                node = parentNode;
+                parentNode = node.Parent;
+            }
+            return node;
+        }
+        public static TData GetRootNodeData<TData>(this IHierarchyNode<TData> node)
+        {
+            return node.GetRootNode().Data;
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> ToFlatNodeList<TData>(this IHierarchyNode<TData> node)
+        {
+            yield return node;
+            foreach (var child in node.GetAllChildrenNodes())
+            {
+                yield return child;
+            }
+        }
+        public static IEnumerable<TData> ToFlatDataList<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var hierarchyNode in node.ToFlatNodeList())
+            {
+                yield return hierarchyNode.Data;
+            }
+        }
+
         public static IEnumerable<THierarchyModel> ToHierarchy<THierarchyModel, TData, TKey>(this IEnumerable<TData> flatList, Func<TData, TKey> idSelector, Func<TData, TKey?> parentIdSelector)
             where THierarchyModel : IHierarchyNode<TData>, new()
         {
