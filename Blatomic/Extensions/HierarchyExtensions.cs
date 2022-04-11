3c426aa2b688410054ef3af34559fe7f96d392ae
Jeremy Buentello <jb09056@georgiasouthern.edu>
2022-04-10 22:01:01 -0400

    Testing all of the hierarchy extension methods... beginning to wrap common linq methods so they traverse the hierarchy. Not sure if this is the best way or not moving forward

--- Blatomic/Extensions/HierarchyExtensions.cs
+++ Blatomic/Extensions/HierarchyExtensions.cs
@@ -37,7 +37,7 @@
                 }
             }
         }
-        public static IEnumerable<TData> GetAllSiblingData<TData>(this IHierarchyNode<TData> node)
+        public static IEnumerable<TData?> GetAllSiblingNodesData<TData>(this IHierarchyNode<TData> node)
         {
             foreach (var sibling in node.GetAllSiblingNodes())
             {
@@ -56,7 +56,7 @@
                 }
             }
         }
-        public static IEnumerable<TData> GetAllChildrenData<TData>(this IHierarchyNode<TData> node)
+        public static IEnumerable<TData?> GetAllChildrenNodesData<TData>(this IHierarchyNode<TData> node)
         {
             foreach (var child in node.GetAllChildrenNodes())
             {
@@ -77,7 +77,7 @@
                 yield return grandParent;
             }
         }
-        public static IEnumerable<TData> GetAllParentsData<TData>(this IHierarchyNode<TData> node)
+        public static IEnumerable<TData?> GetAllParentNodesData<TData>(this IHierarchyNode<TData> node)
         {
             foreach (var parent in node.GetAllParentNodes())
             {
@@ -95,12 +95,12 @@
             }
             return node;
         }
-        public static TData GetRootNodeData<TData>(this IHierarchyNode<TData> node)
+        public static TData? GetRootNodeData<TData>(this IHierarchyNode<TData> node)
         {
             return node.GetRootNode().Data;
         }
 
-        public static IEnumerable<IHierarchyNode<TData>> ToFlatNodeList<TData>(this IHierarchyNode<TData> node)
+        public static IEnumerable<IHierarchyNode<TData>> FromBranchToFlatNodeList<TData>(this IHierarchyNode<TData> node)
         {
             yield return node;
             foreach (var child in node.GetAllChildrenNodes())
@@ -108,24 +108,107 @@
                 yield return child;
             }
         }
-        public static IEnumerable<TData> ToFlatDataList<TData>(this IHierarchyNode<TData> node)
+        public static IEnumerable<TData?> FromBranchToFlatNodeDataList<TData>(this IHierarchyNode<TData> node)
         {
-            foreach (var hierarchyNode in node.ToFlatNodeList())
+            foreach (var hierarchyNode in node.FromBranchToFlatNodeList())
             {
                 yield return hierarchyNode.Data;
             }
         }
 
-        public static IEnumerable<THierarchyModel> ToHierarchy<THierarchyModel, TData, TKey>(this IEnumerable<TData> flatList, Func<TData, TKey> idSelector, Func<TData, TKey?> parentIdSelector)
+        public static IEnumerable<IHierarchyNode<TData>> GetLeafNodes<TData>(this IHierarchyNode<TData> node)
+        {
+            if (node.Children is null || node.Children.Count == 0)
+            {
+                yield return node;
+                yield break;
+            }
+            foreach (var child in node.Children)
+            {
+                foreach (var leaf in child.GetLeafNodes())
+                {
+                    yield return leaf;
+                }
+            }
+        }
+        public static IEnumerable<TData?> GetLeafNodesData<TData>(this IHierarchyNode<TData> node)
+        {
+            foreach (var leafNode in node.GetLeafNodes())
+            {
+                yield return leafNode.Data;
+            }
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> ToFlatNodeList<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes)
+        {
+            foreach (var node in hierarchyNodes)
+            {
+                yield return node;
+                foreach (var childNode in node.GetAllChildrenNodes())
+                {
+                    yield return childNode;
+                }
+            }
+        }
+        public static IEnumerable<TData?> ToFlatDataList<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes)
+        {
+            foreach (var hierarchyNode in hierarchyNodes.ToFlatNodeList())
+            {
+                yield return hierarchyNode.Data;
+            }
+        }
+
+        public static int Count<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes)
+        {            
+            return Enumerable.Count(hierarchyNodes.ToFlatNodeList());
+        }
+
+        public static int Count<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes, Func<IHierarchyNode<TData>, bool> predicate)
+        {
+            return Enumerable.Count(hierarchyNodes.ToFlatNodeList(), predicate);
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> Except<TData>(this IEnumerable<IHierarchyNode<TData>> first, IEnumerable<IHierarchyNode<TData>> second)
+        {
+            return Enumerable.Except(first.ToFlatNodeList(), second.ToFlatNodeList());
+        }
+
+        public static bool All<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes, Func<IHierarchyNode<TData>, bool> predicate)
+        {
+            return Enumerable.All(hierarchyNodes.ToFlatNodeList(), predicate);
+        }
+
+        public static IEnumerable<IHierarchyNode<TData>> Where<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes, Func<IHierarchyNode<TData>, bool> predicate)
+        {
+            return Enumerable.Where(hierarchyNodes.ToFlatNodeList(), predicate);
+        }
+
+        public static IHierarchyNode<TData> First<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes, Func<IHierarchyNode<TData>, bool> predicate)
+        {
+            return Enumerable.First(hierarchyNodes.ToFlatNodeList(), predicate);
+        }
+
+        public static IHierarchyNode<TData>? FirstOrDefault<TData>(this IEnumerable<IHierarchyNode<TData>> hierarchyNodes, Func<IHierarchyNode<TData>, bool> predicate)
+        {
+            return Enumerable.FirstOrDefault(hierarchyNodes.ToFlatNodeList(), predicate);
+        }
+
+        /// <summary>
+        /// Pass in a flat list with id selectors to get back a list of hierarchy nodes in the form of a hierarchy
+        /// </summary>
+        /// <typeparam name="THierarchyModel">The hierarchy node model that will be used</typeparam>
+        /// <typeparam name="TData">The data type of the flat list</typeparam>
+        /// <typeparam name="TKey">The data type of the keys for the items id and parent id</typeparam>
+        /// <param name="flatList">The flat list that will be transformed to a hierarchy</param>
+        /// <param name="idSelector">Returns the unique identifier for this item</param>
+        /// <param name="parentIdSelector">Returns the parent id for this item</param>
+        /// <returns>A hierarchical model for each item wrapped in a hierarchy node to be able to traverse the hierarchy</returns>
+        private static IEnumerable<THierarchyModel> ToHierarchyInternal<THierarchyModel, TData, TKey>(this IEnumerable<TData> flatList, Func<TData, TKey> idSelector, Func<TData, TKey?> parentIdSelector)
             where THierarchyModel : IHierarchyNode<TData>, new()
         {
-#pragma warning disable CS8604 // Possible null reference argument.
-#pragma warning disable CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint.
-            var lookup = flatList.Where(item => item is not null)
+            var lookup = flatList.Where(item => item is not null && idSelector(item) is not null)
                                                             .Select(f => new THierarchyModel { Data = f })
                                                             .ToDictionary(h => idSelector(h.Data));
-#pragma warning restore CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint.
-#pragma warning restore CS8604 // Possible null reference argument.
 
             if (lookup is null || lookup.Count == 0)
             {
@@ -134,9 +217,7 @@
 
             foreach (var item in lookup.Values)
             {
-#pragma warning disable CS8604 // Possible null reference argument.
                 var parentId = parentIdSelector(item.Data);
-#pragma warning restore CS8604 // Possible null reference argument.
                 if (parentId is null || !lookup.TryGetValue(parentId, out var parent))
                 {
                     yield return item;
@@ -146,6 +227,13 @@
                 parent.Children.Add(item);
                 item.Parent = parent;
             }
+        }
+        
+        public static IEnumerable<THierarchyModel> ToHierarchy<THierarchyModel, TData, TKey>(this IEnumerable<TData> flatList, Func<TData, TKey> idSelector, Func<TData, TKey?> parentIdSelector)
+            where THierarchyModel : IHierarchyNode<TData>, new()
+        {
+            // NOTE: If we do not call ToList() here then the children will not be present in future calls
+            return flatList.ToHierarchyInternal<THierarchyModel, TData, TKey>(idSelector, parentIdSelector).ToList();
         }
 
         public static IEnumerable<IHierarchyNode<TData>> ToHierarchy<TData, TKey>(this IEnumerable<TData> flatList, Func<TData, TKey> idSelector, Func<TData, TKey?> parentIdSelector)
